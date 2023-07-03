fca86df4bd21efdb3f7b9d7d6488befefbd97ec5
thukyaw11 <helloeleven968@gmail.com>
2023-07-04 00:24:35 +0630

    feat: first commit

---
+++ src/components/Business.jsx
@@ -0,0 +1,15 @@
+import { features } from '../constants'
+import styles, { layout } from '../style'
+import Button from './Button'
+
+const Business = () => 
+
+    <section id='features' className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>You do the business, <br className='sm:block hidden'/> we’ll handle the money.</h2>
+      </div>
+    </section>
+  
+
+
+export default Business
